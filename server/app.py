e3f5ad7bd4457e7532a440c6826b919933a0caf1
Brayan Vanz de Oliveira <brayanvanzdeoliveira@gmail.com>
2024-11-01 22:41:55 -0300

    Server: Servidor websocket com Flask
    Implementado servidor Flask para conexão entre múltiplos host.
    Templates contém um index.html sample para enviar e receber mensagens.

---
+++ server/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(msg):
+    print('Message: ' + msg)
+    send(msg, broadcast=True)
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
