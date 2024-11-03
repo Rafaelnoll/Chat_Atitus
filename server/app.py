5382a6eac678fab51ed590cb56f06b4c60a0808c
Jean <jeanhefler@outlook.com>
2024-11-03 15:50:27 -0300

    server: rota register

--- server/app.py
+++ server/app.py
@@ -1,18 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, jsonify, request
 from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+users = []
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.post('/login')
+@app.route('/login', methods = ['POST'])
 def inputLogin():
     login = request.json
     return login
+
+@app.route('/register',methods= ['POST'])
+def user():
+    data = request.get_json()
+    user = {
+        'name': data['name'],
+        'password': data['password']
+        }
+    users.append(user)
+    return users
 
 @socketio.on('message')
 def handle_message(msg):
